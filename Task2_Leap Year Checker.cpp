478fe188471ccc69363a373fdafe420074161637
Shamima050 <shamimagaffar1705@gmail.com>
2024-09-24 14:09:37 +0600

    Create Task2_Leap Year Checker.cpp
    Wrttie a program that takes a year as input and checks whether it is a leap year or not using if-else statement.

---
+++ Task2_Leap Year Checker.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    printf("enter the number: ");
+    scanf("%d", N);
+
+    if(N%2==0){
+        printf("%d is even", N);
+    }
+    else {
+        printf("%d is even", N)
+    }
+    return 0;
+}
